e403d4ffa5a6faf1810fb6f991132ed65625ccbd
daniel <daniel940613@gmail.com>
2018-02-17 01:02:16 +0800

    Push code - 2018/02/17

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+@include('includes.head')
+
+<body>
+<div class="container">
+    <header class='container'>
+        @include('includes.menu')
+    </header>
+    @include('includes.menu')
+    <main role="main">
+        @yield('content')
+    </main>
+
+    @include('includes.jsfooter')
+    <footer>
+        @include('includes.footer')
+    </footer>
+</div>
+</body>
+</html>
